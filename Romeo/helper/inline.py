442a0050a4699cb9293d0280920db4a903616616
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-02-25 12:34:38 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- Romeo/helper/inline.py
+++ Romeo/helper/inline.py
@@ -14,7 +14,7 @@ looters = None
 
 
 def paginate_help(page_number, loaded_modules, prefix):
-    number_of_rows = 5
+    number_of_rows = 7
     number_of_cols = 3
     global looters
     looters = page_number
